05c5039fd15465721562facf4a4aee407f21306f
MujahidEmon <mujahidulislam.official@gmail.com>
2025-04-27 00:43:52 +0600

    upto limitation of data types

---
+++ io.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main(){
+    int inp;
+    printf("Enter a number: ");
+    scanf("%d", &inp);
+    printf("\nEntered Value is: %d", inp);
+    return 0;
+}
---
+++ limitation.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+int main()
+{
+    int n = 10000000000 ;
+    printf("%d", n);
+    return 0;
+} 
