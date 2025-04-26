28ec33b9ebcbf7db4ded10b5afec4bdf7a6084ef
MujahidEmon <mujahidulislam.official@gmail.com>
2025-04-26 22:50:01 +0600

    first commit

---
+++ variable.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main()
+{
+    // int n = 5;
+    // printf("Integer is %d", n);
+
+    // float m = 2.02;
+    // printf("Float is %f", m);
+
+    
+    // float m = 2.02;
+    // printf("Float is %0.3f", m);  // Point er por 3 ghor print korbe
+
+
+    // char dile just ektai print korbe, ek er odhik dile last er ta print korbe
+
+    char c = 'm';
+    printf("Printing char %c", c);
+
+    return 0;
+}
